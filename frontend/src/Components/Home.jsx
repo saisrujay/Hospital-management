6e520d6f37e060b6e6ccfaee4267a749ab4d5687
Karthik Alladi <karthikalladi@Karthiks-MacBook-Pro.local>
2023-06-16 16:32:02 +0530

    updated login page

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,13 +1,15 @@
 import './Home.css'
 import {Link} from "react-router-dom";
-export default function Register() {
+export default function Home() {
     return (
         <div className='home'>
             <div className="button">
                 <button className="b1">  
                 <Link to="/login">Existing User</Link>
                  </button>
-                <button className="b1">New User</button>
+                <button className="b1">
+                    <Link to='/register'>New User</Link>
+                </button>
             </div>
         </div>
     )
