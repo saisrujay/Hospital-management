70ab87b74cf487e6ba153ffe9f2a48fc21c7d519
linga66 <lingasreeja86@gmail.com>
2023-06-16 12:06:10 +0530

    updated mongo

--- Backend/models/users.js
+++ Backend/models/users.js
@@ -3,7 +3,28 @@ const mongo = require('mongoose');
 
 const userSchema = mongo.Schema(
     {
-
+        name: {
+            type: String,
+            required: true
+          },
+          age: {
+            type: String,
+            required: true
+          },
+          gender: {
+            type: String,
+            required: true
+          },
+          uhid: {
+            type: String,
+            required: true,
+            unique: true
+          },
+          upid: {
+            type: String,
+            required: true,
+            unique: true
+          }
     }
 )
 
