a28851f1b4e296f0adfea681785d4db9d5230ea4
Dheeraj Reddy <pvdheerajreddy@gmail.com>
2023-06-20 15:45:49 +0530

    Added toaster in registration page

--- frontend/src/Components/Register.js
+++ frontend/src/Components/Register.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
-import "./Login.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './Login.css';
 
 const Register = () => {
   const navigate = useNavigate();
@@ -22,8 +24,19 @@ const Register = () => {
       // Handle successful registration
       console.log('Registration successful:', response.data);
 
-      // Redirect to /login
-      navigate('/login');
+      // Show notification box with name and uhid
+      toast.success(
+        <div>
+          <p><b>Registration successful</b></p>
+          <p>Name: {name}</p>
+          <p>UHID: {response.data.uhid}</p>
+          <p><b>Please save UHID</b></p>
+        </div>
+      );
+      // Redirect to /login after a delay
+      setTimeout(() => {
+        navigate('/login');
+      }, 10000);
     } catch (error) {
       // Handle registration failure
       console.error('Registration failed:', error);
@@ -32,58 +45,69 @@ const Register = () => {
 
   return (
     <div id="card">
-    <div id="card-content">
-      <div id="card-title">
-        <h2>REGISTER</h2>
-      <form class="form"onSubmit={handleFormSubmit}>
-            <label for="user-email" style={{paddingTop:'13px', textAlign: 'left'}}>
-            &nbsp;Name
-          </label>
-          <input
-            type="text"
-            id="user-email" class="form-content"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        <div class="form-border"></div>
-          <label htmlFor="user-email" style={{ paddingTop: '26px', textAlign: 'left' }}>
-            &nbsp;Age
-          </label>
-          <input
-            type="number"
-            id="user-email" class="form-content"
-            value={age}
-            onChange={(e) => setAge(e.target.value)}
-            required
-          />
-
-          <div class="form-border"></div>
-          <label htmlFor="user-email" style={{ paddingTop: '29px', textAlign: 'left' }}>
-            &nbsp;Gender
-          </label>
-          <select
-            id="user-email" class="form-content"
-            value={gender}
-            onChange={(e) => setGender(e.target.value)}
-            required
-          >
-            <option value=""></option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            <option value="other">Other</option>
-          </select>
-          <div class="form-border"></div>
-        <input id="submit-btn" type="submit" name="submit" value="REGISTER" />
-      </form>
-      <div style={{ display: 'flex', justifyContent: 'center' }} className='buttons-container'>
-          <Link to="/">Home</Link>
+      <div id="card-content">
+        <div id="card-title">
+          <h2>REGISTER</h2>
+          <form class="form" onSubmit={handleFormSubmit}>
+            <label for="user-email" style={{ paddingTop: '13px', textAlign: 'left' }}>
+              &nbsp;Name
+            </label>
+            <input
+              type="text"
+              id="user-email"
+              class="form-content"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+            <div class="form-border"></div>
+            <label htmlFor="user-email" style={{ paddingTop: '26px', textAlign: 'left' }}>
+              &nbsp;Age
+            </label>
+            <input
+              type="number"
+              id="user-email"
+              class="form-content"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+              required
+            />
+            <div class="form-border"></div>
+            <label htmlFor="user-email" style={{ paddingTop: '29px', textAlign: 'left' }}>
+              &nbsp;Gender
+            </label>
+            <select
+              id="user-email"
+              class="form-content"
+              value={gender}
+              onChange={(e) => setGender(e.target.value)}
+              required
+            >
+              <option value=""></option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
+            </select>
+            <div class="form-border"></div>
+            <input id="submit-btn" type="submit" name="submit" value="REGISTER" />
+          </form>
+          <div style={{ display: 'flex', justifyContent: 'center' }} className='buttons-container'>
+            <Link to="/">Home</Link>
+          </div>
+        </div>
       </div>
-
-    </div>
-    </div>
+      <ToastContainer
+        position="top-center"
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnHover
+      />
     </div>
   );
 };
+
+
+
 
 export default Register;
